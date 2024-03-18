746a1b75245ec20d3579b6f6aaeeaae5e66038dc
luka-danger <lukathevegan@gmail.com>
2024-03-18 09:17:08 -0600

    Add myLibrary array to book.js
    Create skeleton HTML file

--- book.js
+++ book.js
@@ -1,3 +1,7 @@
+// Store all books inside myLibrary 
+const myLibrary = [];
+
+// Book Constructor 
 function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
@@ -15,6 +19,7 @@ function Book(title, author, pages, hasRead) {
     }
 }
 
+/*
 const theHobbit = new Book("The Hobbit", "JRR Tolkien", 295, true);
 
 const theFellowship = new Book("The Fellowship of The Ring", "JRR Tolkien", 327, false);
@@ -25,4 +30,4 @@ console.log(theFellowship.info())
 console.log(Object.getPrototypeOf(Book.prototype) === Object.prototype);
 
 console.log(theHobbit.valueOf());
-console.log(theFellowship.valueOf());
+console.log(theFellowship.valueOf()); */ 
