5cf7d9bed30970fcd06633493890c3bf8867c546
luka-danger <lukathevegan@gmail.com>
2024-03-27 11:54:46 -0600

    Create Add Book To Library Function
    Remove classes

--- book.js
+++ book.js
@@ -1,46 +1,40 @@
-class Book {
-    // Book Constructor
-    constructor(title, author, pages, hasRead) {
+// Create new Library Object Array
+let myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.hasRead = hasRead;
-    }
-}
-
-class Library {
-    // Library Constructor to store books
-    constructor() {
-        this.library = []
-    }
-
-    addBookToLibrary(newBook) {
-        this.library.push(newBook)
-    }
-
-    displayBooks() {
-    }
 }
 
 
-// Create new Library Object Array
-const myLibrary = new Library();
 
+function addBookToLibrary() {
+    let title = document.querySelector('#title').value; 
+    let author = document.querySelector('#author').value; 
+    let pages = document.querySelector('#pages').value; 
+    let hasRead = document.querySelector('#hasRead').value; 
+    let newBook = new Book(title, author, pages, hasRead);
+    console.log(newBook)
+}
+
+    
 // Add Default Data to Display in Table
 const theHobbit = new Book("The Hobbit", "JRR Tolkien", 320, "Yes");
 const theRings = new Book("The Lord of the Rings", "JRR Tolkien", 1178, "No");
 
-myLibrary.addBookToLibrary(theHobbit);
-myLibrary.addBookToLibrary(theRings);
-
-
-const dialog = document.querySelector('dialog');
-const showButton = document.querySelector('#add-book-btn');
-
 
 // "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
+const dialog = document.querySelector('dialog');
+const addNewBook = document.querySelector('#add-book-btn');
+
+addNewBook.addEventListener("click", () => {
   dialog.showModal();
 });
 
+document.querySelector('#add-book-form').addEventListener('submit', () => {
+    event.preventDefault();
+    addBookToLibrary();
+})
 
